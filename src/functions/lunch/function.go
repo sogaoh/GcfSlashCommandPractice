88a434b772c690d2c75356421049507569e960eb
sogaoh <sogaoh@ant-in-giant.work>
2019-05-03 21:10:29 +0900

    Page 44 まで （v2）

--- src/functions/lunch/function.go
+++ src/functions/lunch/function.go
@@ -4,16 +4,43 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
-func Lunch(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
-	log.Printf("%v", string(b))
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
+ func Lunch(w http.ResponseWriter, r *http.Request) {
+	 if r.Method != "POST" {
+		 e := "Method Not Allowed."
+		 log.Println(e)
+		 w.WriteHeader(http.StatusMethodNotAllowed)
+		 w.Write([]byte(e))
+		 return
+	 }
+
+	 b, err := ioutil.ReadAll(r.Body)
+	 if err != nil {
+		 log.Printf("ReadAllError: %v\n", err)
+		 w.WriteHeader(http.StatusInternalServerError)
+		 w.Write([]byte(err.Error()))
+		 return
+	 }
+
+	 parsed, err := url.ParseQuery(string(b))
+	 if err != nil {
+		 log.Printf("ParseQueryError: %v\n", err)
+		 w.WriteHeader(http.StatusInternalServerError)
+		 w.Write([]byte(err.Error()))
+		 return
+	 }
+
+	 if parsed.Get("token") != os.Getenv("SLACK_TOKEN") {
+		 e := "Unauthorized Token."
+		 log.Printf(e)
+		 w.WriteHeader(http.StatusUnauthorized)
+		 w.Write([]byte(e))
+		 return
+	 }
+
+	 w.WriteHeader(http.StatusOK)
+	 w.Write([]byte(parsed.Get("text")))
+ }
